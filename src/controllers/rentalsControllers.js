8b1f2fd381c85bf1d9299b7fb76f2ebae2829ad2
layconjohn <layconjohn@gmail.com>
2022-10-02 16:42:06 -0300

    feat: verificação se a quantidade alugada do jogo corresponde ao estoque disponível

--- src/controllers/rentalsControllers.js
+++ src/controllers/rentalsControllers.js
@@ -17,6 +17,11 @@ const postRental = async (req, res) => {
             return res.sendStatus(400);
         }
 
+        const rentals = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1;', [game.rows[0].id])
+        if (rentals.rows.length > game.rows[0].stockTotal) {
+            return res.sendStatus(400);
+        }
+
         const originalPrice = daysRented * game.rows[0].pricePerDay;
 
         await connection.query('INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7);', [customerId, gameId, daysRented, rentDate, null, originalPrice, null]);
