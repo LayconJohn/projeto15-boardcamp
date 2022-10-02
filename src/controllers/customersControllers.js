b95da38214a73b4ad5fd81d1265d70fe4841b45b
layconjohn <layconjohn@gmail.com>
2022-10-02 00:40:00 -0300

    feat: Rota GET /customers/:id

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -2,6 +2,41 @@ import { connection } from "../db/database.js";
 
 import { customerSchema } from "../schemas/customersSchema.js";
 
+const getCustomers = async (req, res) => {
+    const cpf = req.query.cpf;
+
+    try {
+        let customers; 
+        if (cpf) {
+            customers = await connection.query('SELECT * FROM customers WHERE cpf LIKE \'%$1%\';', [cpf]);
+        }
+        customers = await connection.query('SELECT * FROM customers;')
+
+        res.status(200).send(customers.rows);
+        
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+};
+
+const getCustomersById = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const customer = await connection.query('SELECT * FROM customers WHERE id = $1', [id]);
+        if (customer.rows.length === 0) {
+            return res.sendStatus(404);
+        }
+
+        res.status(200).send(customer.rows[0]);
+    } catch (error) {
+        console.erroe(error);
+        res.sendStatus(500);
+    }
+};
+
+
 const postCustomer = async (req, res) => {
     const { name, phone, cpf, birthday } = req.body;
 
@@ -30,4 +65,4 @@ const postCustomer = async (req, res) => {
 
 };
 
-export {postCustomer};
+export {postCustomer, getCustomers, getCustomersById};
--- src/routers/customersRouters.js
+++ src/routers/customersRouters.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { postCustomer } from "../controllers/customersControllers.js";
+import { postCustomer, getCustomers, getCustomersById } from "../controllers/customersControllers.js";
 
 const router = Router();
 
+router.get("/customers", getCustomers);
+router.get("/customers/:id", getCustomersById);
 router.post("/customers", postCustomer);
 
 export default router;
